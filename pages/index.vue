db259cf0380ec587235fee75d73d18dfe7c835b8
matt.ganz <matt.ganz@gmail.com>
2021-06-09 07:44:36 -0400

    layout changes to use grid system correctly

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,72 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="6" sm="5" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          <i>Out-of-Service</i>
-        </v-card-title>
-        <v-card-text>
-          <audio src="~/assets/gong.wav" crossorigin="anonymous"></audio>
-          <p>
-            I am malfunctioning. All avenues available to the stable person are no longer 
-            viable options for me. Active listening, compromise and empathy
-            -- ALL, out the window. Communication has deteriorated to such a point that all I can do is vent
-            my most raw thoughts here in the hopes that flushing it down the abyss of anonymity will purge me of
-            my demons. Where therapy fails, a virtual word toilet will serve as a sufficient replacement. 
-          </p>
-          <p>
-            When it comes to you, and me-and-you, this is me at my most merciful.  
-          </p>
-          <v-textarea
-            auto-grow
-            autofocus
-            background-color="#333"
-            color="#fff">
-          </v-textarea>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="normal"
-            nuxt
-            @click="onSubmit()"
-          >
-            Purge
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-    <v-col cols="7" sm="8" md="6">
-      <div class="text-center">
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          CONFESSIONS
-        </v-card-title>
-        <v-list max-height="85vh" class="overflow-y-auto">
-          <v-card class="mb-8" v-for="post in posts.data" :key="post.id">
+  <div>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" md="6">
+          <v-card>
+            <v-card-title class="headline">
+              <i>Out-of-Service</i>
+            </v-card-title>
             <v-card-text>
-              {{post.body}}
+              <audio src="~/assets/gong.wav" crossorigin="anonymous"></audio>
+              <p>
+                I am malfunctioning. All avenues available to the stable person are no longer 
+                viable options for me. Active listening, compromise and empathy
+                -- ALL, out the window. Communication has deteriorated to such a point that all I can do is vent
+                my most raw thoughts here in the hopes that flushing it down the abyss of anonymity will purge me of
+                my demons. Where therapy fails, a virtual word toilet will serve as a sufficient replacement. 
+              </p>
+              <p>
+                When it comes to you, and me-and-you, this is me at my most merciful.  
+              </p>
+              <v-textarea
+                auto-grow
+                autofocus
+                background-color="#333"
+                color="#fff"
+                v-model="confession">
+              </v-textarea>
             </v-card-text>
+
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="normal"
+                nuxt
+                @click="onSubmit()"
+                :disabled="!confession.length"
+              >
+                Purge
+              </v-btn>
+            </v-card-actions>
           </v-card>
-        </v-list>
-      </v-card>
-    </v-col>
-  </v-row>
+        </v-col>
+        <v-col cols="12" md="6">
+          <div class="text-center">
+          </div>
+          <v-card>
+            <v-card-title class="headline">
+              CONFESSIONS
+            </v-card-title>
+            <v-list max-height="85vh" class="overflow-y-auto">
+              <v-card class="mb-8" v-for="post in posts.data" :key="post.id">
+                <v-card-text>
+                  {{post.body}}
+                </v-card-text>
+              </v-card>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
   components: {},
   data: () => ({
-    posts:[]
+    posts:[],
+    confession: ""
   }),
   mounted(){
     this.getPosts();
@@ -74,16 +81,17 @@ export default {
       return res;
     },
     onSubmit() {
-      const input = document.querySelector('textarea').value;
-      const res = this.savePost(input); // send message to server
-      res 
-        .then((res) => {
-          this.playSound(); // play sound
-          this.getPosts(); // refresh messages
-        })
-        .catch((err) => {
-          console.log(`Error saving new post: ${err}`);
-        });
+      const input = document.querySelector('textarea').value.textContent;
+      console.log(input);
+      // const res = this.savePost(input); // send message to server
+      // res 
+      //   .then((res) => {
+      //     this.playSound(); // play sound
+      //     this.getPosts(); // refresh messages
+      //   })
+      //   .catch((err) => {
+      //     console.log(`Error saving new post: ${err}`);
+      //   });
     },
     playSound: function(){
         var playPromise = document.querySelector('audio').play();
