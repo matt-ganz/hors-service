dd6ac61d51c4876e5f84777ace4bc07f5fd043f1
Matthew Ganz <matt.ganz@gmail.com>
2023-01-19 00:10:13 -0500

    New build

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,5 +1,5 @@
-export { default as Logo } from '../../components/Logo.vue'
-export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
+export const Logo = () => import('../../components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
+export const VuetifyLogo = () => import('../../components/VuetifyLogo.vue' /* webpackChunkName: "components/vuetify-logo" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
