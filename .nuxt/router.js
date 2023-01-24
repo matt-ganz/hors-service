31a04b78dc99e6bdba7cd5133f0efe2e3ecdc3da
Matthew Ganz <matt.ganz@gmail.com>
2023-01-24 00:18:01 -0500

    new build

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -13,7 +13,7 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: '/hors-service/',
+  base: '/',
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
