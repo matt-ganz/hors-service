cc1e20efcd20bbe9c73cce1c429344a20af584b0
Matthew Ganz <matt.ganz@gmail.com>
2023-01-30 11:08:15 -0500

    new build

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
   telemetry: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
